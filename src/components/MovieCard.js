2c34945e4b1da3852381f691caefd299084a79cf
Luis Rosario <luisalmonter@gmail.com>
2020-12-07 15:40:38 -0500

    adds styling to components

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,12 @@ import styles from './MovieCard.module.css';
 
 function MovieCard(props) {
     return(
+        
         <div className={styles.Card}>
-            {props.result.title}
+
             <img src={`https://image.tmdb.org/t/p/w200${props.result.poster_path}`} />
+
+            <h6 style={{fontSize: 15}}>{props.result.title}</h6>
             
         </div>
     )
