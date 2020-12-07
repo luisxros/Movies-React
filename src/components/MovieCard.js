a136f174c44c5fa21c58c08d5e49d309c7d3f26f
Luis Rosario <luisalmonter@gmail.com>
2020-12-07 14:30:04 -0500

    adds movie component

---
+++ src/components/MovieCard.js
@@ -0,0 +1,12 @@
+import styles from './MovieCard.module.css';
+
+function MovieCard(props) {
+    return(
+        <div className={styles.Card}>
+            Movie
+
+        </div>
+    )
+}
+
+export default MovieCard;
