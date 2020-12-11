21c69af4463e5f59518464eb1c82aa9c0c7d626b
Luis Rosario <luisalmonter@gmail.com>
2020-12-11 17:30:03 -0500

    adds alt for img

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,7 +5,7 @@ function MovieCard(props) {
         
         <div className={styles.Card}>
 
-            <img src={`https://image.tmdb.org/t/p/w200${props.result.poster_path}`} />
+            <img src={`https://image.tmdb.org/t/p/w200${props.result.poster_path}`} alt= "" />
 
             <h6 style={{fontSize: 15}}>{props.result.title}</h6>
             
